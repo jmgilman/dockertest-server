1bce9059fea398a97eb5bb85ccaba7e648f26664
Joshua Gilman <joshuagilman@gmail.com>
2022-03-15 09:50:48 -0700

    build: bumps dependencies

--- src/servers/cloud/localstack.rs
+++ src/servers/cloud/localstack.rs
@@ -123,7 +123,7 @@ mod tests {
     use super::{LocalStackServer, LocalStackServerConfig};
     use crate::Test;
     use std::collections::HashMap;
-    use test_env_log::test;
+    use test_log::test;
 
     #[test]
     fn test_local_stack() {
